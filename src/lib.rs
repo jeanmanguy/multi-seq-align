19c29ecc981aaa11e236c8fb8175ad9aceff1fcc
Jean Manguy <jean@manguy.eu>
2020-08-30 23:27:29 +0100

    cleaning

--- src/lib.rs
+++ src/lib.rs
@@ -1,79 +1,76 @@
-//! ![stability-experimental](https://img.shields.io/badge/stability-experimental-orange.svg)
-//!
-//! A crate to manipulate multiple sequences alignments in Rust.
-//!
-//! Instead of storing aligned sequences as multiple strings, `multi_seq_align` stores bases or residues in [`Alignment`] using a list of characters, like a matrix. This allows easy access to specific rows and columns of the alignment.
-//!
-//! # Usage
-//!
-//! ```rust
-//! # use multi_seq_align::Alignment;
-//!     let mut kappa_casein_fragments_alignment = Alignment::create(
-//!        vec![
-//!            "P06796".to_string(), // Mouse
-//!            "P07498".to_string(), // Human
-//!            "P02668".to_string(), // Cattle
-//!        ],
-//!        vec![
-//!            Some("CASK_MOUSE".to_string()),
-//!            Some("CASK_HUMAN".to_string()),
-//!            Some("CASK_BOVIN".to_string()),
-//!        ],
-//!        &[
-//!            "PAPISKWQSMP".to_string(),
-//!            "HAQIPQRQYLP".to_string(),
-//!            "PAQILQWQVLS".to_string(),
-//!        ],
-//!    ).unwrap();
-//!
-//!    // Let's extract a column of this alignment
-//!    assert_eq!(
-//!        kappa_casein_fragments_alignment.nth(6),
-//!        [Some(&'W'), Some(&'R'), Some(&'W')]
-//!    );
-//!
-//!    // But we also have the aligned sequence for the Platypus
-//!    // Let's add it to the original alignment
-//!    kappa_casein_fragments_alignment.add_aligned_sequence(
-//!        "D0QJA9".to_string(),
-//!        Some("D0QJA9_ORNAN".to_string()),
-//!        "EHQRP--YVLP",
-//!    ).unwrap();
-//!
-//!    // the new aligned sequence has a gap at the 6th position
-//!    assert_eq!(
-//!        kappa_casein_fragments_alignment.nth(6),
-//!        [Some(&'W'), Some(&'R'), Some(&'W'), Some(&'-')]
-//!    );
-//! ```
-//!
-//! # Features
-//!
-//! - Create [`Alignment`] from one or multiple aligned sequences at once (see [`add_aligned_sequence()`] and [`create()`]).
-//! - Extract columns of the alignment (see [`nth()`]).
-//!
-//! This crate is currently in early stage development. I wouldn't recommend using it in production but I am interested in possible ideas to further the developemt of this project. Quite some work needs toi be done to improve the API and make it easy to use in other project.
-//!
-//! # Ideas
-//!
-//! - Computation of conservation scores
-//! - Identification of conserved sites
-//! - Computation of consensus sequence
-//! - Collapse / trim alignment
-//! - Serialisation / Deserialisation of alignment files
-//! - Extract sub-alignments
-//!     - positions
-//!     - motifs
-//!
-//! # Optimisation
-//!
-//! My goal is to reduce the footprint of this crate, there is ome work to do to achieve it. The code will eventually be optimised to be faster and to better use memory.
-//!
-//! [`Alignment`]: struct.Alignment.html
-//! [`nth()`]: struct.Alignment.html#method.nth
-//! [`add_aligned_sequence()`]: struct.Alignment.html#method.add_aligned_sequence
-//! [`create()`]: struct.Alignment.html#method.create
-//!
+/*! ![stability-experimental](https://img.shields.io/badge/stability-experimental-orange.svg)
+
+A crate to manipulate multiple sequences alignments in Rust.
+
+Instead of storing aligned sequences as multiple strings, `multi_seq_align` stores bases or residues in [`Alignment`] using a list of characters, like a matrix. This allows easy access to specific rows and columns of the alignment.
+
+# Usage
+
+```rust
+# use multi_seq_align::Alignment;
+let mut kappa_casein_fragments_alignment = Alignment::create(
+    vec![
+        "P06796".to_string(), // Mouse
+        "P07498".to_string(), // Human
+        "P02668".to_string(), // Cattle
+    ],
+    vec![
+        Some("CASK_MOUSE".to_string()),
+        Some("CASK_HUMAN".to_string()),
+        Some("CASK_BOVIN".to_string()),
+    ],
+    &[
+        "PAPISKWQSMP".to_string(),
+        "HAQIPQRQYLP".to_string(),
+        "PAQILQWQVLS".to_string(),
+    ],
+).unwrap();
+
+// Let's extract a column of this alignment
+assert_eq!(
+    kappa_casein_fragments_alignment.nth(6),
+    [Some(&'W'), Some(&'R'), Some(&'W')]
+);
+
+// But we also have the aligned sequence for the Platypus
+// Let's add it to the original alignment
+kappa_casein_fragments_alignment.add_aligned_sequence(
+    "D0QJA9".to_string(),
+    Some("D0QJA9_ORNAN".to_string()),
+    "EHQRP--YVLP",
+).unwrap();
+
+// the new aligned sequence has a gap at the 6th position
+assert_eq!(
+    kappa_casein_fragments_alignment.nth(6),
+    [Some(&'W'), Some(&'R'), Some(&'W'), Some(&'-')]
+);
+```
+
+# Features
+
+- Create [`Alignment`] from one or multiple aligned sequences at once (see [`add_aligned_sequence()`] and [`create()`]).
+- Extract columns of the alignment (see [`nth()`]).
+This crate is currently in early stage development. I wouldn't recommend using it in production but I am interested in possible ideas to further the developemt of this project. Quite some work needs toi be done to improve the API and make it easy to use in other project.
+# Ideas
+- Computation of conservation scores
+- Identification of conserved sites
+- Computation of consensus sequence
+- Collapse / trim alignment
+- Serialisation / Deserialisation of alignment files
+- Extract sub-alignments
+    - positions
+    - motifs
+
+# Optimisation
+
+My goal is to reduce the footprint of this crate, there is ome work to do to achieve it. The code will eventually be optimised to be faster and to better use memory.
+
+[`Alignment`]: struct.Alignment.html
+[`nth()`]: struct.Alignment.html#method.nth
+[`add_aligned_sequence()`]: struct.Alignment.html#method.add_aligned_sequence
+[`create()`]: struct.Alignment.html#method.create
+*/
 #![warn(clippy::all, clippy::pedantic, clippy::nursery, clippy::cargo)]
 
 mod errors;
@@ -258,13 +255,10 @@ impl Alignment {
             });
         }
 
-        // let mut names = self.names.clone();
         self.names.push(name);
 
-        // let mut descriptions = self.descriptions.clone();
         self.descriptions.push(description);
 
-        // let mut sequences = self.sequences.clone();
         self.sequences.extend(sequence.chars());
 
         self.n_sequences += 1;
@@ -334,7 +328,6 @@ mod tests {
         )
         .unwrap();
 
-        // align =
         align
             .add_aligned_sequence(String::from("added1"), None, "ALRYITAT")
             .unwrap();
